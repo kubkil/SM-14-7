a34bd551be9097145bfdb54c09cbc6b7cc3f93d2
Jakub Kilar <kuba.kilar@gmail.com>
2018-04-13 21:22:48 +0200

    copy components from SM-14-5

---
+++ components/contactform.js
@@ -0,0 +1,28 @@
+const ContactForm = React.createClass({
+  propTypes: {
+    // dlaczego z dużej a nie małej prop
+    contact: React.PropTypes.object.isRequired,
+  },
+  render: function () {
+    return (
+      React.createElement('form', {className: 'contactForm'},
+        React.createElement('input', {
+          type: 'text',
+          placeholder: 'Imię',
+          value: this.props.contact.firstName,
+        }),
+        React.createElement('input', {
+          type: 'text',
+          placeholder: 'Nazwisko',
+          value: this.props.contact.lastName,
+        }),
+        React.createElement('input', {
+          type: 'email',
+          placeholder: 'Email',
+          value: this.props.contact.email,
+        }),
+        React.createElement('button', { type: 'submit' }, "Dodaj kontakt")
+      )
+    )
+  },
+});
